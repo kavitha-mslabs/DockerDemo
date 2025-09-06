5f9773282207fec4e7970225667ec79651462e93
kavitha-mslabs <kavitha.mslabs@co.in>
2025-09-06 11:11:47 +0530

    admin deleted_06-09-2025

--- db_scripts/February_2024/Insert_Closure_Reason.sql
+++
@@ -1,4 +0,0 @@
-insert into closure_reason (id,complaint_code,reason) values (closure_reason_id_seq.nextval,8,'Conductor Stringed')
-insert into closure_reason (id,complaint_code,reason) values (closure_reason_id_seq.nextval,8,'Fault attended. Supply restored')
-insert into closure_reason (id,complaint_code,reason) values (closure_reason_id_seq.nextval,8,'Defective equipment replaced')
-insert into closure_reason (id,complaint_code,reason) values (closure_reason_id_seq.nextval,8,'Cables Buried')
--- db_scripts/aug20221/insert script.sql
+++
@@ -1,4 +0,0 @@
-UPDATE SUB_CATEGORY SET NAME = 'Fluctuation in Voltage' WHERE NAME = 'Voltage Fluctuations';
-
-INSERT INTO SUB_CATEGORY (ID, NAME, CATEGORY_ID) VALUES 
-(SUB_CATEGORY_ID_SEQ.nextval, 'Frequent Interruption',(SELECT ID FROM CATEGORY WHERE NAME = 'Voltage Related'));
